a30c2ef54f6d3017640664a656747263570f210e
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-04 21:38:34 -0300

    Update main.js

--- JS Projects/main.js
+++ JS Projects/main.js
@@ -9,3 +9,4 @@ const deposit = () => {
         if (isNaN(numberDepositAmout) || numberDepositAmout <= 0 ) {
             console.log("Invalid deposit amount, try again")
         } else {
+            return numberDepositAmout;
