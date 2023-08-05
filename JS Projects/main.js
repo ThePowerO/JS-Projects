f350aff6df2b76002730207c8cb2dcd62da637f4
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-04 21:37:29 -0300

    Update main.js

--- JS Projects/main.js
+++ JS Projects/main.js
@@ -5,3 +5,5 @@ const deposit = () => {
     while (true) {
         const DepositAmout = prompt("Insert a deposit amout: ");
         const numberDepositAmout = parseFloat(DepositAmout);
+
+        if (isNaN(numberDepositAmout) || numberDepositAmout <= 0 ) {
