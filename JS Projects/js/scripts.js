a0da42f2ac5489bf8581a4bd53bcb0ae65415ac0
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-06 18:43:50 -0300

    Update scripts.js

--- JS Projects/js/scripts.js
+++ JS Projects/js/scripts.js
@@ -2,3 +2,4 @@
 const todoForm = document.querySelector("#todo-form")
 const todoInput = document.querySelector("#todo-input")
 const todoList = document.querySelector("#todo-list")
+const editForm = document.querySelector("#edit-form")
