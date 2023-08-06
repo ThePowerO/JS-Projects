4b4a56f656f306b998dea5532c1fbda5f84eb593
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-06 18:53:00 -0300

    Update scripts.js

--- JS Projects/js/scripts.js
+++ JS Projects/js/scripts.js
@@ -30,3 +30,4 @@ const saveTodo = (text) => {
     const removeBtn = document.createElement("button")
     removeBtn.classList.add("remove-todo")
     removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(removeBtn);
