62cc1935c0415441c24eebb4656cfcd602e71b9b
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-06 18:52:02 -0300

    Update scripts.js

--- JS Projects/js/scripts.js
+++ JS Projects/js/scripts.js
@@ -24,3 +24,4 @@ const saveTodo = (text) => {
 
     const editBtn = document.createElement("button")
     editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
