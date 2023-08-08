487bf3b6a5b4dcd8d97d9b64f5ad094b390425e5
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-07 21:19:53 -0300

    Update scripts.js

--- To do Project2/js/scripts.js
+++ To do Project2/js/scripts.js
@@ -1,3 +1,4 @@
 // Elements Selection
 const todoForm = document.querySelector("#todo-form")
 const todoInput = document.querySelector("#todo-input")
+const todoList = document.querySelector("#todo-list")
